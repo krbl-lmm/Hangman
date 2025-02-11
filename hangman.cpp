b3f907f43a6b93a79255432e59d2717760610760
krbl-lmm <146442914+krbl-lmm@users.noreply.github.com>
2025-02-11 11:19:09 +0300

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -7,6 +7,7 @@ int main() {
     int guesses = 0, correctGuesses = 0;
     char letter;
     string word, guess0 = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ";
+    bool correct = false;
     cout << "Welcome to Hangman!\nWhat word would you like the player to guess?\n";
     cin >> word;
     int n = word.length();
@@ -17,8 +18,20 @@ int main() {
         cin >> letter;
         for (int i = 0; i < n; i++) {
             if (letter == word[i]) {
-                
+                guess[2 * i] = letter;
+                correctGuesses++;
+                correct = true;
             }
         }
+        if (!correct) guesses++;
+        if (correctGuesses == n) {
+            cout << word << “\nYou won!” << endl;
+            break;
+        }
+        else if (guesses == 9) {
+            cout << word << “\nYou lost!” << endl;
+            break;
+        }
+        correct = false;
     }
 }
